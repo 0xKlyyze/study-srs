a824f9f4d4486ced020ae57924ce588a80b0f1e7
0xKlyyze <mpitaval07@gmail.com>
2025-04-07 21:40:04 +0200

    Enhance card statistics UI with updated SVG icons for better visual representation

--- js/views/chapterFoldersView.js
+++ js/views/chapterFoldersView.js
@@ -798,15 +798,28 @@ class ChapterFoldersView {
                     </div>
                     <div class="card-stats-new">
                         <div class="stat-item-new total-cards" title="Total Cards">
-                            <svg class="stat-icon" viewBox="0 0 32 32"> /* SVG Content */ </svg>
+                            <svg class="card-icon" width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                                        <title>Icon representing a deck of taller flashcards, showing one</title>
+                                        <!-- Back Card (rotated slightly more, taller) -->
+                                        <rect x="6" y="4" width="20" height="24" rx="3" ry="3" fill="none" stroke="currentColor" stroke-width="1.5" transform="rotate(-6 16 16)" /> 
+                                        <!-- Front Card Group (rotated slightly less) -->
+                                        <g transform="rotate(4 13 13)"> 
+                                            <!-- Front Card Outline (taller) -->
+                                            <rect x="4" y="2" width="18" height="22" rx="3" ry="3" fill="none" stroke="currentColor" stroke-width="1.5" />
+                                        </g>
+                                    </svg>
                             <span class="stat-value" data-stat="total">${totalCards}</span>
                         </div>
                         <div class="stat-item-new due-cards" title="Cards Due">
-                            <svg class="stat-icon" viewBox="0 0 24 24"> /* SVG Content */ </svg>
+                            <svg class="stat-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                 <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"/>
+            </svg>
                             <span class="stat-value" data-stat="due">${dueCards}</span>
                         </div>
                         <div class="stat-item-new new-cards" title="New Cards Remaining">
-                            <svg class="stat-icon" viewBox="0 0 24 24"> /* SVG Content */ </svg>
+                            <svg class="stat-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13 7H11v4H7v2h4v4h2v-4h4v-2h-4V7zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
+             </svg>
                             <span class="stat-value" data-stat="new">${remainingNewCards}</span>
                         </div>
                     </div>
